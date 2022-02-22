5d0b726801c14695dfac4e89f370d3b2dca54fdc
涛之雨 <49258735+taozhiyu@users.noreply.github.com>
2022-02-22 10:31:23 +0800

    Update init.js

--- init.js
+++ init.js
@@ -1,14 +1,3 @@
-import * as core from "actions/github-script";
-async function run(){
-  try {
-    core.info(`Hello world`);
-    const eventName = github.context.eventName;
-    core.info(`name is ${eventName}`);
-  } catch (error) {
-    core.setFailed(error.message);
-  }
-}
-
 module.exports = async ({github, context, core}) => {
   const {SHA} = process.env
   const commit = await github.rest.repos.getCommit({
@@ -16,6 +5,6 @@ module.exports = async ({github, context, core}) => {
     repo: context.repo.repo,
     ref: `${SHA}`
   })
+  core.exportVariable('commit', commit)
   core.exportVariable('author', commit.data.commit.author.email)
-  run();
 }
