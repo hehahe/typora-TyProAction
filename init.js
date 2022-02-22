bb70edd772cf7fb4c222b33502c4408043facee7
涛之雨 <49258735+taozhiyu@users.noreply.github.com>
2022-02-22 10:41:00 +0800

    Update init.js

--- init.js
+++ init.js
@@ -1,10 +1,12 @@
 module.exports = async ({github, context, core}) => {
-  const {SHA} = process.env
-  const commit = await github.rest.repos.getCommit({
-    owner: context.repo.owner,
-    repo: context.repo.repo,
-    ref: `${SHA}`
-  })
-  core.exportVariable('commit', commit)
-  core.exportVariable('author', commit.data.commit.author.email)
+//   const {SHA} = process.env
+//   const commit = await github.rest.repos.getCommit({
+//     owner: context.repo.owner,
+//     repo: context.repo.repo,
+//     ref: `${SHA}`
+//   })
+  core.exportVariable('github', github)
+  core.exportVariable('context', context)
+  core.exportVariable('core', core)
+//   core.exportVariable('author', commit.data.commit.author.email)
 }
