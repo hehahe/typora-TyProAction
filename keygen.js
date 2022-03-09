58f06d3db21ac1a3dd77d74e49443f926b0ec223
涛之雨 <49258735+taozhiyu@users.noreply.github.com>
2022-03-10 00:07:14 +0800

    Update keygen.js

--- keygen.js
+++ keygen.js
@@ -1,57 +1,47 @@
-module.exports = async ({github, context, core,KEYGEN_JS_CODE}) => {
-//   if(/^update[\d\.]+$/.test(context.payload.issue.title)){
-//     if(context.payload.issue.author_association!=='OWNER'){
-//       console.log('权限检测未通过，已驳回')
-//       await github.rest.issues.createComment({
-//         issue_number: context.issue.number,
-//         owner: context.repo.owner,
-//         repo: context.repo.repo,
-//         body: 'sorry, only the **OWNER** can use this keyword.\n'+
-//               'but if it do have an update in TyPro,\n'+
-//               'you can notice me if you have my contact details,\n'+
-//               'otherwise maybe you can **ONLY** waiting until I remembered to updated it\n'+
-//               '(maybe you can build your own repositories by clone?).\n\n'+
-//               '抱歉，只有**所有者**(@taozhiyu)可以使用此关键字。\n'+
-//               '但是，如果 TyPro 确实有更新，如果你有我的联系方式，你可以通知我；\n'+
-//               '否则你可能**只能**等到我记得更新它\n'+
-//               '（也许你可以通过克隆构建自己的存储库？）。'
-//       });
-//       await github.rest.issues.update({
-//         owner: context.repo.owner,
-//         repo: context.repo.repo,
-//         issue_number: context.issue.number,
-//         state: 'closed'
-//       });
-//       return
-//     }
-//     console.log('权限检测确认')
-//     const urlList=context.payload.issue.body.match(/https?:\/\/[^)]+/g);
-//     const fileVersion=context.payload.issue.title.substr(6)
-//     console.log(urlList)
-//     await cacheFiles(core,'asar.asar',fileVersion,urlList[0])
-// //     fs.writeFileSync('newFile'+_t,result.data)
-//   }else 
-    console.log(context)
-  if(context.payload.issue.title==='keygen'){
-    const info=context.payload.issue.body
-    console.log(KEYGEN_JS_CODE)
-      console.log(typeof KEYGEN_JS_CODE)
-    console.log(KEYGEN_JS_CODE("eyJ2Ijoid2lufDEuMS40IiwiaSI6InRmcnpDT3JFdnEiLCJsIjoiVEFPWkhJWVUgfCB0YW96aCB8IFdpbmRvd3MifQ==","taozhiyu@52pojie.cn","imtaozhiyu"))
-  }else{
-      await github.rest.issues.createComment({
-        issue_number: context.issue.number,
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        body: '[指令匹配错误]'
-      });
-      await github.rest.issues.update({
-        owner: context.repo.owner,
-        repo: context.repo.repo,
-        issue_number: context.issue.number,
-        state: 'closed',
-        labels: '🤔invalid/无效的😒'
-      });
-      return
+module.exports = async ({
+  github,
+  context,
+  core,
+  KEYGEN_JS_CODE
+}) => {
+  async function endWithComment(words, isok) {
+    await github.rest.issues.createComment({
+      issue_number: context.issue.number,
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      body: words || '指令匹配错误\n\ncommand match error'
+    });
+    await github.rest.issues.update({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      issue_number: context.issue.number,
+      state: 'closed',
+      labels: [isok ? '☑️keygen/注册机🎉' : '🤔invalid/无效的😒']
+    });
+    return;
   }
-  
-}
+
+  if (context.payload.issue.title === 'keygen') {
+    try {
+      const info = context.payload.issue.body;
+      const commMatch = info.replace(/\r/g, '').match(/<!--.+-->/s);
+
+      if (commMatch) {
+        const conf = comm[0].split('\n').filter(i => !i.match(/：|<!--|-->/));
+
+        if (conf.length === 3) {
+          const key = KEYGEN_JS_CODE(...conf);
+            
+      endWithComment(`您的激活码为/Your activation code is:\n\n\`${key}\``,true);
+        }
+      }
+
+      endWithComment('无法正确匹配到配置信息\n\nCan not match the configuration information correctly.');
+      return;
+    } catch (error) {
+      endWithComment('激活码计算过程中发生错误\n\nAn error occurred during activation code calculation');
+    }
+  } else {
+    await endWithComment();
+  }
+};
