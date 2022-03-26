4d72e442ff5eb5d3b47ffec825246e1b1a1f97bb
涛之雨 <49258735+taozhiyu@users.noreply.github.com>
2022-03-26 14:07:09 +0800

    Update keygen.js

--- keygen.js
+++ keygen.js
@@ -1,8 +1,10 @@
+const crypto = require('crypto');
 module.exports = async ({
   github,
   context,
   core,
-  KEYGEN_JS_CODE
+  KEYGEN_JS_CODE,
+  PRIVATE_KEY
 }) => {
   async function endWithComment(words, isok) {
     await github.rest.issues.createComment({
@@ -20,7 +22,12 @@ module.exports = async ({
     });
     return;
   }
-
+  function encode (MachineCode,email,license){
+    var Base64={table:["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","0","1","2","3","4","5","6","7","8","9","+","/"],UTF16ToUTF8:function(r){for(var e=[],t=r.length,h=0;h<t;h++){var o=r.charCodeAt(h);if(o>0&&o<=127)e.push(r.charAt(h));else if(o>=128&&o<=2047){var a=192|o>>6&31,i=128|63&o;e.push(String.fromCharCode(a),String.fromCharCode(i))}else if(o>=2048&&o<=65535){a=224|o>>12&15,i=128|o>>6&63;var c=128|63&o;e.push(String.fromCharCode(a),String.fromCharCode(i),String.fromCharCode(c))}}return e.join("")},UTF8ToUTF16:function(r){var e=[],t=r.length,h=0;for(h=0;h<t;h++){var o=r.charCodeAt(h);if(0==(o>>7&255))e.push(r.charAt(h));else if(6==(o>>5&255)){var a=(31&o)<<6|63&(i=r.charCodeAt(++h));e.push(Sting.fromCharCode(a))}else if(14==(o>>4&255)){var i;a=(255&(o<<4|(i=r.charCodeAt(++h))>>2&15))<<8|((3&i)<<6|63&r.charCodeAt(++h));e.push(String.fromCharCode(a))}}return e.join("")},encode:function(r){if(!r)return"";for(var e=this.UTF16ToUTF8(r),t=0,h=e.length,o=[];t<h;){var a=255&e.charCodeAt(t++);if(o.push(this.table[a>>2]),t==h){o.push(this.table[(3&a)<<4]),o.push("==");break}var i=e.charCodeAt(t++);if(t==h){o.push(this.table[(3&a)<<4|i>>4&15]),o.push(this.table[(15&i)<<2]),o.push("=");break}var c=e.charCodeAt(t++);o.push(this.table[(3&a)<<4|i>>4&15]),o.push(this.table[(15&i)<<2|(192&c)>>6]),o.push(this.table[63&c])}return o.join("")},decode:function(r){if(!r)return"";for(var e=r.length,t=0,h=[];t<e;)code1=this.table.indexOf(r.charAt(t++)),code2=this.table.indexOf(r.charAt(t++)),code3=this.table.indexOf(r.charAt(t++)),code4=this.table.indexOf(r.charAt(t++)),c1=code1<<2|code2>>4,c2=(15&code2)<<4|code3>>2,c3=(3&code3)<<6|code4,h.push(String.fromCharCode(c1)),64!=code3&&h.push(String.fromCharCode(c2)),64!=code4&&h.push(String.fromCharCode(c3));return this.UTF8ToUTF16(h.join(""))}};
+    var mc=JSON.parse(Base64.decode(MachineCode));
+    var signInfo={fingerprint: mc.i, email , license, type: '1'};
+    return JSON.stringify(signInfo);
+  }
   if (context.payload.issue.title === 'keygen') {
     try {
       const info = context.payload.issue.body;
@@ -30,7 +37,10 @@ module.exports = async ({
         const conf = commMatch[0].split('\n').filter(i => !i.match(/：|<!--|-->/));
 
         if (conf.length === 3) {
-          const key = KEYGEN_JS_CODE(...conf);
+//           const key = KEYGEN_JS_CODE(...conf);
+          const encodeData2 = crypto.privateEncrypt(PRIVATE_KEY, Buffer.from(encode(...conf))).toString('base64');
+          console.log("encode2: ", encodeData2)
+          const key=encodeData2;
           await endWithComment(`您的离线激活码为/Your offline activation code is:
 
 \`+${key}\`
