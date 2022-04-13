c8f845ab8a95e6e44cd0a5d9aca69dbbd8b57333
涛之雨 <49258735+taozhiyu@users.noreply.github.com>
2022-04-14 00:02:46 +0800

    Update mainUpdate.py

--- scripts/mainUpdate.py
+++ scripts/mainUpdate.py
@@ -104,7 +104,7 @@ def test_inject(version: str):
     # 复制asar文件到根目录
     shutil.copyfile(os.path.join(appDir, "../app.asar"), os.path.join(rootPath, "app.asar"))
 
-    zip_file = zipfile.ZipFile(f'app-file-V{version}.zip','w')
+    zip_file = zipfile.ZipFile(os.path.join(rootPath,f'app-file-V{version}.zip'),'w')
     # 把zfile整个目录下所有内容，压缩为new.zip文件
     zip_file.write("app.asar",compress_type=zipfile.ZIP_DEFLATED)
     zip_file.close()
